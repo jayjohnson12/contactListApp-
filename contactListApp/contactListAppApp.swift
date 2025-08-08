a0e9db07006dfa08f48965f9872f0eb2d503ad37
Javonne Johnson <jjavonne13@gmail.com>
2025-08-08 00:09:27 -0400

    Initial Commit

---
+++ contactListApp/contactListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  contactListAppApp.swift
+//  contactListApp
+//
+//  Created by Javonne Johnson on 8/8/25.
+//
+
+import SwiftUI
+
+@main
+struct contactListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
